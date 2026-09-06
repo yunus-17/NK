6b1f6311d3267756d25e7bb557dc4feaac6f70f8
yunus-17 <mohammedyunus7010@gmail.com>
2026-09-06 11:12:45 +0530

    Configure frontend API endpoint

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,7 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const API = "http://3.234.250.114/api";
+export const API = import.meta.env.VITE_API_URL || "http://3.234.250.114/api";
 
 export function cn(...inputs) {
     return twMerge(clsx(inputs))
